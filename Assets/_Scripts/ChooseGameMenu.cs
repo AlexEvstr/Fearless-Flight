fe1aeb8b0608338393615b50702343537a3cefa2
Alexander Evstratov <alexdrugev@me.com>
2024-09-04 17:14:43 +0300

    Add enter scene.

--- Assets/_Scripts/ChooseGameMenu.cs
+++ Assets/_Scripts/ChooseGameMenu.cs
@@ -1,11 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChooseGameMenu : MonoBehaviour
 {
     private void Start()
     {
-        
+        Time.timeScale = 1;
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    public void OpenGhostMenu()
+    {
+        SceneManager.LoadScene("GhostMenu");
+    }
+
+    public void OpenPlaneMenu()
+    {
+        SceneManager.LoadScene("PlaneMenu");
     }
 }
