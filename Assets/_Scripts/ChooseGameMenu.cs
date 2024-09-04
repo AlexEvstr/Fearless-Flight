37c8f607a7385118d5c5cd8a5d37c5a8fd586ccd
Alexander Evstratov <alexdrugev@me.com>
2024-09-04 15:20:40 +0300

    Create project.

---
+++ Assets/_Scripts/ChooseGameMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseGameMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+}
