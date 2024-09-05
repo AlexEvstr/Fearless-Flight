b7f3cbb03b03b28aca8b6b13e28f7f4898cfd157
Alexander Evstratov <alexdrugev@me.com>
2024-09-05 23:30:04 +0300

    Add preGame screen.

---
+++ Assets/_Scripts/MainGameGhostController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGameGhostController : MonoBehaviour
+{
+    [SerializeField] private Image _blackScreen;
+
+    private void Start()
+    {
+        StartCoroutine(ShowGame());
+    }
+
+    private IEnumerator ShowGame()
+    {
+        yield return new WaitForSeconds(2f);
+        float aIndex = 1.0f;
+        while (aIndex > 0)
+        {
+            _blackScreen.color = new Color(0, 0, 0, aIndex);
+            aIndex -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
---
+++ Assets/_Scripts/PreGameGhostController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreGameGhostController : MonoBehaviour
+{
+    [SerializeField] private Image _background;
+    [SerializeField] private Sprite[] _backgroundSprites;
+    [SerializeField] private GameObject _preGame;
+    [SerializeField] private Image _whiteScreen;
+    [SerializeField] private Image _blackScreen;
+    [SerializeField] private GameObject _mainGame;
+
+    private void OnEnable()
+    {
+        _preGame.SetActive(true);
+        _background.sprite = _backgroundSprites[PlayerPrefs.GetInt("BackgroundIndex", 0)];
+    }
+
+    private void Start()
+    {
+        StartCoroutine(ShowWhiteScreen());
+    }
+
+    private IEnumerator ShowWhiteScreen()
+    {
+        float aIndex = 0f;
+        yield return new WaitForSeconds(7.0f);
+
+        while (aIndex < 1)
+        {
+            _whiteScreen.color = new Color(1, 1, 1, aIndex);
+            aIndex += 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+        _blackScreen.color = new Color(0, 0, 0, 1);
+        _mainGame.SetActive(true);
+        _preGame.SetActive(false);
+    }
+}
