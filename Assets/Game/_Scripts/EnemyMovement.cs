842a8a47eea432cb7b732c9619d99e8743c646d3
Alexander Evstratov <alexdrugev@me.com>
2024-09-10 13:05:13 +0300

    Add enemy movement.

---
+++ Assets/Game/_Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private float _speed = 2.0f;
+
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(MoveToPlayer(player));
+    }
+
+    private IEnumerator MoveToPlayer(GameObject player)
+    {
+        while(true)
+        {
+            Vector2 currentPosition = transform.position;
+            Vector2 targetPosition = player.transform.position;
+            transform.position = Vector2.MoveTowards(currentPosition, targetPosition, _speed * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+    }
+}
